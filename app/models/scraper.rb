409164b795878243b76c5e17526145e11065b014
Eva <yi.zheng1709@gmail.com>
2021-03-09 22:16:04 -0500

    iterates through the array to find the name of each pokemon

--- app/models/scraper.rb
+++ app/models/scraper.rb
@@ -19,7 +19,14 @@ class Scraper
         end
         array = array[37,934]
         array.collect {|pokemon| pokemon[:profile].prepend("https://www.pokemon.com")}
+        array.each do |pokemon|
+            page = Nokogiri::HTML(open(pokemon[:profile]))
+            # pk = Pokemon.create
+            name = page.css(".pokedex-pokemon-pagination-title").children.text.strip.split(' ')[0]
 
+
+            
+        end 
     end
 
 end
