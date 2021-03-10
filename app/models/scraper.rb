82bfcc09a83049d6ce55674a1036b07b07e6a318
Eva <yi.zheng1709@gmail.com>
2021-03-09 22:13:24 -0500

    add every pokemon's individual page as a key-value hash to a big array

--- app/models/scraper.rb
+++ app/models/scraper.rb
@@ -6,7 +6,18 @@ class Scraper
     def get_page
         doc = Nokogiri::HTML(open("https://www.pokemon.com/us/pokedex/"))
         doc2 = doc.css("li")
-
+        array = []
+        doc2.each do |li|
+            # binding.pry
+            if li.css("a").attribute("href")
+                hash = {
+                    :profile => li.css("a").attribute("href").children.text
+                }
+                array << hash
+            end 
+            # binding.pry
+        end
+        
     end
 
 end
