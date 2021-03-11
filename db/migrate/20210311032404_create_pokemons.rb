520c9c8d32fbad167e0f17c6edfec3a9d1bca6a8
Eva <yi.zheng1709@gmail.com>
2021-03-10 22:24:53 -0500

    uses model generator to generate a Pokemon model and its corresponding database file

---
+++ db/migrate/20210311032404_create_pokemons.rb
@@ -0,0 +1,17 @@
+class CreatePokemons < ActiveRecord::Migration[6.1]
+  def change
+    create_table :pokemons do |t|
+      t.string :name
+      t.string :types
+      t.string :weaknesses
+      t.string :abilities
+      t.text :description
+      t.string :image
+      t.string :category
+      t.string :height
+      t.string :weight
+
+      t.timestamps
+    end
+  end
+end
