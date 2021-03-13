7518961d6d4509023535246dbb947dcaee6b531e
Eva <yi.zheng1709@gmail.com>
2021-03-13 00:25:43 -0500

    adds in pokemon controller; moves the index and show action to controller (they were originally in pokemon model class

---
+++ app/controllers/pokemon_controller.rb
@@ -0,0 +1,10 @@
+class PokemonController < ApplicationController
+    def index
+        @pokemons = Pokemon.all
+    end
+
+    def show 
+        @pokemon = Pokemon.find_by(id: params[:id])
+    end
+
+end
--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,9 +1,3 @@
 class Pokemon < ApplicationRecord
-    def index
-        @pokemons = Pokemon.all
-    end
-
-    def show 
-        @pokemon = Pokemon.find_by(id: params[:id])
-    end
+    
 end
