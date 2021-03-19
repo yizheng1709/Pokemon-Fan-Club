696070efe45ba5bd3f5e99692d5fa3e4323409f5
Eva <yi.zheng1709@gmail.com>
2021-03-19 02:10:43 -0400

    moves the if statement that checks if the pokemon has multiple abilities to the model class to maintain separation of concerns

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,3 +1,8 @@
 class Pokemon < ApplicationRecord
-    
+    def check_abilities 
+        if self.abilities.include?("[")
+            self.abilities = self.abilities.split(",").collect{|a| a.gsub!(/\W+/, ' ').strip}.join(", ")
+        end
+        self.abilities
+    end
 end
