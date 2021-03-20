34b715bdd9fe570235fd831afff683c04f489eb5
Eva <yi.zheng1709@gmail.com>
2021-03-19 21:11:29 -0400

    defines the resources for teams and user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :pokemons
-  resources :users, only: [:show]
+  resources :pokemons, :teams 
+  resources :users, only: [:show] #show user's homepage; link to every team(if any)
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
