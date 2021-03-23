df06a2d98bd2cb4ab8a2316026aacaee99ea5ff3
Eva <yi.zheng1709@gmail.com>
2021-03-23 14:30:03 -0400

    adds nested routes for /users/:id/teams

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :pokemons, :teams 
-  resources :users, only: [:show, :new, :create] #show user's homepage; link to every team(if any)
+  resources :users, only: [:show, :new, :create] do #show user's homepage; link to every team(if any)
+    resources :teams 
+  end 
   get '/', to: 'users#login'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
