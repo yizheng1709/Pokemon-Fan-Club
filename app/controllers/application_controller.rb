f58784826a4db1b647afe5ac3957a962a18ff1bc
Eva <yi.zheng1709@gmail.com>
2021-03-24 21:34:58 -0400

    defines current_user in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+    def current_user 
+        @user = User.find_by_id(session[:user_id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :new, :create] do #show user's homepage; link to every team(if any)
     resources :teams 
   end 
-  get '/', to: 'session#home'
+  root 'session#home'
   get '/login', to: 'session#login'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
